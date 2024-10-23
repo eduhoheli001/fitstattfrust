a74db617f1a2e1f3c73502cf3aac42c047af6896
Lara Hedenig <eduhedlar001@fh-kaernten.at>
2024-10-23 17:29:35 +0200

    init data

---
+++ android/app/src/main/kotlin/com/example/fittstattfrust/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fittstattfrust
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+import 'game.dart';
+
+void main() => runApp(Fludo());
+
+class Fludo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FludoGame(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
